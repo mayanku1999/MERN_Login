83f9c6395b8e5ff5ec49ce26ed6e0d352e20b96e
Mayank Upadhyay <37012441+mayanku1999@users.noreply.github.com>
2020-10-26 23:44:20 +0530

    Changes Done!

---
+++ Client/build/precache-manifest.58054358bfb44a00c4c44eea0666404e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d94356c6c953764146ee64343dba499",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b48f915799c647b95bc9",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "995266310fc84adf8c30",
+    "url": "/static/js/2.a6ea5532.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.a6ea5532.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b48f915799c647b95bc9",
+    "url": "/static/js/main.f61d62a7.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  }
+]);
---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI: 'mongodb+srv://new_user_0:qwerty12345@cluster0.hbvd6.mongodb.net/LoginSystem',
+  secretOrKey: "secret"
+};
